0266fc34583a56e4e1ac4948ce70c2d0eb9f911f
shex-tor <shexgts@gmail.com>
2026-03-20 14:51:46 +0300

    no1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express      = require("express");
 const path         = require("path");
-const { respond }  = require("./brain");
+const { respond }  = require("./engine/brain");
 
 const app  = express();
 const PORT = process.env.PORT || 3000;
