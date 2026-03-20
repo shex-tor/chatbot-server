dabbc1818f5b654dfecacd04810f26ffdad4ccb8
shex-tor <shexgts@gmail.com>
2026-03-20 15:07:59 +0300

    del-path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express      = require("express");
 const path         = require("path");
-const { respond }  = require("./engine/brain");
+const { respond }  = require("./brain");
 
 const app  = express();
 const PORT = process.env.PORT || 3000;
